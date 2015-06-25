14c9af7286fcbe63c8ff3bcab05210b21cb02a6c
Thoriq <monsterkode@gmail.com>
2015-06-25 15:54:53 +0700

    db

--- chat/chat.sql
+++ chat/chat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 23, 2015 at 04:51 AM
+-- Generation Time: Jun 25, 2015 at 08:58 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -27,32 +27,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `room` (
-`id` bigint(20) unsigned NOT NULL,
+  `id` bigint(20) unsigned NOT NULL,
   `name` varchar(100) NOT NULL,
-  `owner` bigint(20) unsigned NOT NULL,
-  `idroom` int(11) NOT NULL,
+  `idowner` bigint(20) unsigned NOT NULL,
+  `username` varchar(30) NOT NULL,
   `password` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-`id` bigint(20) unsigned NOT NULL,
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`) VALUES
-(1, 'user1'),
-(2, 'user2'),
-(3, 'user3');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -62,37 +42,7 @@ INSERT INTO `user` (`id`, `name`) VALUES
 -- Indexes for table `room`
 --
 ALTER TABLE `room`
- ADD PRIMARY KEY (`id`), ADD KEY `owner` (`owner`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `room`
---
-ALTER TABLE `room`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=62;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `room`
---
-ALTER TABLE `room`
-ADD CONSTRAINT `room_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ ADD PRIMARY KEY (`id`), ADD KEY `owner` (`idowner`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
